5d3c6b84be4757c61ea5344493c667e473085ff4
Alberto Garcia <alb.garciam@gmail.com>
2019-01-04 16:41:06 +0100

    Navigates to root view when clicking on selected item

--- AnimatedTabBar/Classes/AnimatedTabBar.swift
+++ AnimatedTabBar/Classes/AnimatedTabBar.swift
@@ -113,7 +113,9 @@ open class AnimatedTabBar: CommonUIView {
 extension AnimatedTabBar : AnimatedTabBarViewDelegate {
     func didTapped(on item: AnimatedTabBarView) {
         if item.isSelected { // move to root
-            
+            if let nav = selected?.associatedController as? UINavigationController {
+                nav.popToRootViewController(animated: true)
+            }
         } else { // replace view
             selectItem(at: item.position)
         }
